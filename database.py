ffd7947e67f9a0b6bbd2f2740460ba2b7458cd4a
Harmandeep singh <singhbrarharmandeep0@gmail.com>
2026-09-09 23:46:23 +0530

    updating

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ import sqlite3
 
 
 def create_database():
+
     connection = sqlite3.connect("password_manager.db")
+
     cursor = connection.cursor()
 
     cursor.execute("""
@@ -15,17 +17,39 @@ def create_database():
     """)
 
     connection.commit()
+
     connection.close()
 
 
-def add_password(account, username, password):
+def add_password(account, username, encrypted_password):
+
     connection = sqlite3.connect("password_manager.db")
+
     cursor = connection.cursor()
 
     cursor.execute("""
         INSERT INTO passwords (account, username, password)
         VALUES (?, ?, ?)
-    """, (account, username, password))
+    """, (account, username, encrypted_password))
 
     connection.commit()
+
     connection.close()
+
+
+def get_passwords():
+
+    connection = sqlite3.connect("password_manager.db")
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, account, username, password
+        FROM passwords
+    """)
+
+    passwords = cursor.fetchall()
+
+    connection.close()
+
+    return passwords
