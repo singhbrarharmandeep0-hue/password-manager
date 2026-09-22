b26e0ea9c1756467bd961e58ef401d35f30c09a5
Harmandeep singh <singhbrarharmandeep0@gmail.com>
2026-09-22 23:42:23 +0530

    upda

--- encryption.py
+++ encryption.py
@@ -5,32 +5,61 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+
+ITERATIONS = 600000
+
+
 def generate_salt():
+
     return os.urandom(16)
+
+
 def derive_key(master_password, salt):
 
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
-        iterations=600000
+        iterations=ITERATIONS
     )
 
     key = base64.urlsafe_b64encode(
-        kdf.derive(master_password.encode())
+        kdf.derive(
+            master_password.encode()
+        )
     )
 
     return key
 
 
-def create_fernet(master_password, salt):
+def create_verifier(master_password, salt):
 
     key = derive_key(
         master_password,
         salt
     )
 
-    return Fernet(key)
+    return key
+
+
+def verify_master_password(
+    master_password,
+    salt,
+    stored_verifier
+):
+
+    try:
+
+        key = derive_key(
+            master_password,
+            salt
+        )
+
+        return key == stored_verifier
+
+    except Exception:
+
+        return False
 
 
 def encrypt_password(
@@ -39,10 +68,12 @@ def encrypt_password(
     salt
 ):
 
-    fer = create_fernet(
+    key = derive_key(
         master_password,
         salt
     )
+
+    fer = Fernet(key)
 
     encrypted = fer.encrypt(
         password.encode()
@@ -57,10 +88,12 @@ def decrypt_password(
     salt
 ):
 
-    fer = create_fernet(
+    key = derive_key(
         master_password,
         salt
     )
+
+    fer = Fernet(key)
 
     decrypted = fer.decrypt(
         encrypted_password.encode()
